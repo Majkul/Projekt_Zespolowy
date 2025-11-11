56abed2b9ccc0ddab5807fe16da64b5be1b046c8
Korellek <korelleks@gmail.com>
2025-11-11 19:24:59 +0100

    zaktualizowanie .gitignore - usuniecie trackingu na .vs, docker/, bin/, obj/ i appsettings.json. Podstawowe tworzenie oferty i handling plików i tagów

--- ProjektZespolowyGr3/Migrations/20251111170557_tagi-i-pliki.Designer.cs
+++ ProjektZespolowyGr3/Migrations/20251111170557_tagi-i-pliki.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using ProjektZespolowyGr3.Models;
@@ -11,9 +12,11 @@ using ProjektZespolowyGr3.Models;
 namespace ProjektZespolowyGr3.Migrations
 {
     [DbContext(typeof(MyDBContext))]
-    partial class MyDBContextModelSnapshot : ModelSnapshot
+    [Migration("20251111170557_tagi-i-pliki")]
+    partial class tagiipliki
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ ProjektZespolowyGr3/Models/DbModels/ListingPhoto.cs
@@ -0,0 +1,15 @@
+﻿namespace ProjektZespolowyGr3.Models.DbModels
+{
+    public class ListingPhoto
+    {
+        public int Id { get; set; }
+        public bool IsFeatured { get; set; } = false;
+
+        public int ListingId { get; set; }
+        public Listing Listing { get; set; }
+
+        public int UploadId { get; set; }
+        public Upload Upload { get; set; }
+    }
+
+}
