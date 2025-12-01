b283baca7b7ec5a8e64fda4244904b6363c72fd6
Korellek <korelleks@gmail.com>
2025-12-01 19:15:22 +0100

    podstawowe przegladanie ofert

---
+++ ProjektZespolowyGr3/Models/ViewModels/BrowseListingsViewModel.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using ProjektZespolowyGr3.Models.DbModels;
+using System.ComponentModel.DataAnnotations;
+
+namespace ProjektZespolowyGr3.Models.ViewModels
+{
+    public class BrowseListingsViewModel
+    {
+        public Listing Listing { get; set; }
+        public int ListingId { get; set; }
+        public User Seller { get; set; }
+        public int SellerId { get; set; }
+
+        public string PhotoUrl { get; set; }
+        public double AverageRating { get; set; }
+        public int ReviewCount { get; set; }
+        public string SellerName { get; set; }
+    }
+}
