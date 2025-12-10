ed358122e009e6ab79645187a2cef4dc6c5d06cf
SoloBrainPixy <72761418+Itranar@users.noreply.github.com>
2025-12-10 14:18:06 +0100

    generated actual controllers and views
    also changed base64image

---
+++ ProjektZespolowyGr3/Controllers/User/UserProfilesController.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using ProjektZespolowyGr3.Models;
+using ProjektZespolowyGr3.Models.DbModels;
+
+namespace ProjektZespolowyGr3.Controllers.User
+{
+    public class UserProfilesController : Controller
+    {
+        private readonly MyDBContext _context;
+
+        public UserProfilesController(MyDBContext context)
+        {
+            _context = context;
+        }
+
+        // GET: UserProfiles
+        [Route("UserProfile")]
+        public async Task<IActionResult> Index(string username)
+        {
+            var founduser = await FindUserProfile(username);
+
+            if (founduser.IsNullOrEmpty())
+            {
+                founduser = founduser;
+            }
+
+            return View(founduser);
+        }
+
+        private async Task<List<Models.DbModels.UserProfile>> FindUserProfile(string username)
+        {
+
+            var user = _context.UserProfiles.AsQueryable();
+
+            user = user.Where(c =>
+            c._User.Username == username);
+
+            return await user.ToListAsync();
+
+        }
+
+        // GET: UserProfiles/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var userProfile = await _context.UserProfiles
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (userProfile == null)
+            {
+                return NotFound();
+            }
+
+            return View(userProfile);
+        }
+
+        // GET: UserProfiles/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: UserProfiles/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,base64Avatar,base64Banner,Description")] UserProfile userProfile)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(userProfile);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(userProfile);
+        }
+
+        // GET: UserProfiles/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var userProfile = await _context.UserProfiles.FindAsync(id);
+            if (userProfile == null)
+            {
+                return NotFound();
+            }
+            return View(userProfile);
+        }
+
+        // POST: UserProfiles/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,base64Avatar,base64Banner,Description")] UserProfile userProfile)
+        {
+            if (id != userProfile.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(userProfile);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!UserProfileExists(userProfile.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(userProfile);
+        }
+
+        // GET: UserProfiles/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var userProfile = await _context.UserProfiles
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (userProfile == null)
+            {
+                return NotFound();
+            }
+
+            return View(userProfile);
+        }
+
+        // POST: UserProfiles/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var userProfile = await _context.UserProfiles.FindAsync(id);
+            if (userProfile != null)
+            {
+                _context.UserProfiles.Remove(userProfile);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool UserProfileExists(int id)
+        {
+            return _context.UserProfiles.Any(e => e.Id == id);
+        }
+    }
+}
