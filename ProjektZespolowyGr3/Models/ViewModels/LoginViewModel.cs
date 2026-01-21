626ec1aeca3685a9ad22e89112b0d06a24550090
Piotr Grochowski <89135@student.pb.edu.pl>
2026-01-21 15:04:33 +0100

    adding feedback to login and register

--- ProjektZespolowyGr3/Models/ViewModels/LoginViewModel.cs
+++ ProjektZespolowyGr3/Models/ViewModels/LoginViewModel.cs
@@ -1,8 +1,13 @@
 ﻿namespace ProjektZespolowyGr3.Models.ViewModels
 {
+    using System.ComponentModel.DataAnnotations;
+
     public class LoginViewModel
     {
+        [Required(ErrorMessage = "Login jest wymagany.")]
         public string Login { get; set; }
+
+        [Required(ErrorMessage = "Hasło jest wymagane.")]
         public string Password { get; set; }
     }
 }
